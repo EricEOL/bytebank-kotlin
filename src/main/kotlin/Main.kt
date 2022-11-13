902e28d48460899c52ce9314ec2f746f1501695f
EricEOL <lopes.eric051@gmail.com>
2022-11-12 23:29:08 -0300

    Loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,9 +13,45 @@ fun variables() {
     println("Titular: $titular");
     println("Conta: $numeroConta | Saldo: $saldo");
 
+    testaConditions(saldo);
+    testaLoops();
+
+}
+
+fun testaConditions(saldo: Double) {
     when {
         saldo > 0.0 -> println("Saldo positivo");
         saldo == 0.0 -> println("Saldo zerado");
         else -> println("Saldo negativo");
     }
 }
+
+fun testaLoops() {
+    for(i in 1..5) {
+        println("Using range: $i");
+    }
+
+    for(i in 5 downTo 1) {
+        println("Using downTo: $i")
+    }
+
+    for(i in 10 downTo 1 step 2) {
+        println("Using steps to jump in 2: $i")
+    }
+
+    for(i in 10 downTo 1 step 2) {
+        println("Using break in the middle of for: $i");
+        if(i == 4) {
+            break;
+        }
+    }
+
+    var i = 0;
+    while (i <= 5) {
+        println("Using while $i")
+        i++;
+    }
+}
+
+
+
