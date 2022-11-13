dd50afe17e301c63edfebb37a41c87307ad8dcb3
EricEOL <lopes.eric051@gmail.com>
2022-11-12 21:05:54 -0300

    Learning when expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,5 +5,17 @@ fun main() {
 
 fun variables() {
     val titular: String = "Rebecca Lopes";
+    val numeroConta: Int = 1000;
+    var saldo: Double = 0.0;
+    saldo = 100.00;
+    saldo = 0.0;
+
     println("Titular: $titular");
+    println("Conta: $numeroConta | Saldo: $saldo");
+
+    when {
+        saldo > 0.0 -> println("Saldo positivo");
+        saldo == 0.0 -> println("Saldo zerado");
+        else -> println("Saldo negativo");
+    }
 }
